4b2812d9da0eccb8245ed9dd3a59edfa004d13fc
David-104 <muxu@ucm.es>
2024-09-16 17:09:49 +0200

    botones de aparecer y desaparecer

--- Ejercicio1/script.js
+++ Ejercicio1/script.js
@@ -3,11 +3,20 @@ function devuelveTextoDeAlerta() {
 }
 
 function desaparece(nombre) {
-	let buttons = document.querySelectorAll(nombre);
+	let buttons = document.getElementsByName(nombre);
   for(let i = 0; i < buttons.length; ++i){
-    if(buttons[i].style.visibility=='visible'){
-      button[i].style.visibility='hidden';
+    if(buttons[i].style.visibility==='visible'){
+      buttons[i].style.visibility='hidden';
     }
-    else button[i].style.visibility='visible';
+    else buttons[i].style.visibility='visible';
+  }
+}
+
+function colores(nombre){
+  let elems = document.getElementsByClass(nombre);
+  for(let i = 0; i < elems.length; ++i){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    elems[i].color = "#" + randomColor;
+    elems[i].color.innerHTML = "#" + randomColor;
   }
 }
