dd644f32c6bad4caee949456c37c5f94c81c60e4
David-104 <muxu@ucm.es>
2024-09-16 16:21:02 +0200

    mas cambios

--- Ejercicio1/script.js
+++ Ejercicio1/script.js
@@ -3,11 +3,11 @@ function devuelveTextoDeAlerta() {
 }
 
 function desaparece(nombre) {
-	var button = document.getElementsByName(nombre);
-  for(let i = 0; i < button.length; ++i){
-    if(button.style.visibility=='visible'){
-      button.style.visibility='hidden';
+	let buttons = document.querySelectorAll(nombre);
+  for(let i = 0; i < buttons.length; ++i){
+    if(buttons[i].style.visibility=='visible'){
+      button[i].style.visibility='hidden';
     }
-    else button.style.visibility='visible';
+    else button[i].style.visibility='visible';
   }
 }
