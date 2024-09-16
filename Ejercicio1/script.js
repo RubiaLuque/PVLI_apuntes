534813e47d86b4736d78c978dda260413c1bd13a
David-104 <muxu@ucm.es>
2024-09-16 15:53:46 +0200

    cambios web

--- Ejercicio1/script.js
+++ Ejercicio1/script.js
@@ -1,8 +1,13 @@
 function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
+  return "Alerta";
 }
 
 function desaparece(nombre) {
-	var button = document.getElementById(nombre);
-  button.style.visibility='hidden';
+	var button = document.getElementsByName(nombre);
+  for(let i = 0; i < button.length; ++i){
+    if(button.style.visibility=='visible'){
+      button.style.visibility='hidden';
+    }
+    else button.style.visibility='visible';
+  }
 }
